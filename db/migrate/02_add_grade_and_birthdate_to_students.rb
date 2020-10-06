1cb9581d2f0ef9098e8ce824d3557e88a4541335
Paris Dolfman <parisdolfman@gmail.com>
2020-10-06 19:35:18 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
-    add_column do |t|
+    add_column :students do |t|
       t.integer :grade
       t.string :birthdate
     end
